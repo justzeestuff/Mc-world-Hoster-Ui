f43ec45f119941ffeff71ad86702246724274793
justzeestuff <justsomezeestuff@gmail.com>
2026-07-19 01:47:23 +0400

    World Listing vector based function

---
+++ src/include/WorldLister.hpp
@@ -0,0 +1,18 @@
+#include <filesystem>
+#include "include.hpp"
+
+namespace fs = std::filesystem;
+
+std::vector<fs::path> WorldLister(){
+    std::vector<fs::path> dirs = {};
+    fs::path path = "/home/zee/Desktop/McServer";    
+
+    int i = 1;
+    for(const auto& item : fs::directory_iterator(path)){
+        i++;
+        std::cout << i << '.' << item.path().filename() << '\n';
+
+        dirs.push_back({item.path()});
+    }
+    return dirs;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
+#include <filesystem>
+
+// hand written includes
 #include "include/include.hpp"
+#include "include/WorldLister.hpp"
 
 int main(){
-    std ::cout << "works";
+    while (true)
+    {
+        std::string entry = "Create World [0] - Use Existing World [1]";
+        int client;
+
+        std::cout << entry << " :";
+        std::cin >> client;
+        
+        if(client == 1){
+            WorldLister();
+        }
+    }
+    
     return 0;
 }
